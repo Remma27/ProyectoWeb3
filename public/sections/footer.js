f02b47d27d3d2e57a3ebab9d9d66532f8c8e6b71
Remma27 <emmanuelrsolano27@gmail.com>
2023-11-12 19:36:03 -0600

    Header y Footer Alejandro

--- public/sections/footer.js
+++ public/sections/footer.js
@@ -1,7 +1,28 @@
 class Footer extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `<h4>FOOTER</h4>`
+        this.innerHTML = `<div class="footer-container">
+                <div class="footer-section">
+               
+                    <strong>Contacto:</strong><br/>
+                    <a href="mailto:registro.pacifico@utn.ac.cr?subject=Solicito información"> eMail: registro.pacifico@utn.ac.cr</a><br />
+                    <a href="web:www.utn.ac.cr?subject=Solicito información"> Sitio Web Oficial: www.utn.ac.cr</a><br />
+                    Central Telefónica: 2435-5000
+                </div>
+                
+                <div class="footer-section">
+                    ¡Plataforma para subir trabajos de investigación<br/>
+                    para la Universidad Técnica Nacional!<br/>
+                    © 2015 UTN Todos los Derechos Reservados
+                </div>
+                
+                <div class="footer-section">
+                    <a href="https://www.facebook.com/UniversidadTecnicaNacional" class="social-icon"><i class="fa fa-2x fa-facebook"></i></a>
+                    <a href="https://twitter.com/utncostarica?lang=es" class="social-icon"><i class="fa fa-2x fa-twitter"></i></a>
+                    <a href="https://www.youtube.com/@UTNCostaRica" class="social-icon"><i class="fa fa-2x fa-youtube" ></i></a>
+                    <a href="https://www.instagram.com/utncostarica/" class="social-icon"><i class="fa fa-2x fa-instagram"></i></a>
+                </div>
+            </div>`
     }
 }
 
--- public/sections/menu.js
+++ public/sections/menu.js
@@ -1,7 +1,29 @@
 class Menu extends HTMLElement {
     constructor() {
         super();
-        this.innerHTML = `<h4>MENU</h4>`
+        this.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="images/logo.svg" alt="" width="50%"  class="d-inline-block align-text-top">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.html">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.html">Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>`
     }
 }
 
